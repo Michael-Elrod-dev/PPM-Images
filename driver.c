e61dcfe193e50740236747937f8a32dcc56b2fcd
Michael Elrod <117390129+Michael-Elrod-dev@users.noreply.github.com>
2023-04-02 11:25:39 -0400

    Add files via upload

---
+++ driver.c
@@ -0,0 +1,46 @@
+/******************
+ * Michael Elrod
+ * mselrod
+ * mselrod@clemson.edu
+ * Lab 9 005
+*******************/
+#include <stdio.h>
+#include "ppmUtil.h"
+
+int main(int argc, char *argv[]) {
+
+    // Check if the correct number of arguments are provided.
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <input_ppm_file> <output_ppm_file>\n", argv[0]);
+        return 1;
+    }
+
+    // Open the input file for reading.
+    FILE *inputFile = fopen(argv[1], "rb");
+    if (!inputFile) {
+        perror("Error opening input file");
+        return 2;
+    }
+
+    // Open the output file for reading.
+    FILE *outputFile = fopen(argv[2], "wb");
+    if (!outputFile) {
+        perror("Error opening output file");
+        fclose(inputFile);
+        return 3;
+    }
+
+    // Read the PPM image from the input file.
+    header_t header;
+    pixel_t *pixels = read(inputFile, &header);
+
+    // Write the PPM image to the output file.
+    write(outputFile, header, pixels);
+
+    // Close the input and output files and free allocated memory.
+    fclose(inputFile);
+    fclose(outputFile);
+    freeMemory(pixels);
+
+    return 0;
+}
---
+++ ppmUtil.c
@@ -0,0 +1,78 @@
+/******************
+ * Michael Elrod
+ * mselrod
+ * mselrod@clemson.edu
+ * Lab 9 005
+*******************/
+#include "ppmUtil.h"
+
+// Helper function to check for comments and ignore them.
+void ckComment(FILE* fp) {
+    int c;
+    // Consume any whitespace
+    while (isspace(c = fgetc(fp)));
+
+    // Skip any comment lines.
+    while (c == '#') {
+        while (fgetc(fp) != '\n');
+        while (isspace(c = fgetc(fp)));
+    }
+
+    // Put the non-comment character back into the file stream.
+    ungetc(c, fp);
+}
+
+// Reads the header information from the PPM file.
+void readHeader(FILE* fp, header_t* header) {
+    fscanf(fp, "%2s", header->type);
+    ckComment(fp);
+    fscanf(fp, "%u", &header->width);
+    ckComment(fp);
+    fscanf(fp, "%u", &header->height);
+    ckComment(fp);
+    fscanf(fp, "%u", &header->maxVal);
+    fgetc(fp);
+}
+
+// Reads the pixel data from the PPM file.
+pixel_t* readPixels(FILE* fp, header_t header) {
+    size_t pixelCount = header.width * header.height;
+    pixel_t* pixels = allocatePixMemory(header);
+    fread(pixels, sizeof(pixel_t), pixelCount, fp);
+    return pixels;
+}
+
+// Writes the header information to the output PPM file.
+void writeHeader(FILE* fp, header_t header) {
+    fprintf(fp, "%s\n", header.type);
+    fprintf(fp, "%u %u\n", header.width, header.height);
+    fprintf(fp, "%u\n", header.maxVal);
+}
+
+// Writes the pixel data to the output PPM file.
+void writePixels(FILE* fp, pixel_t* pixels, header_t header) {
+    size_t pixelCount = header.width * header.height;
+    fwrite(pixels, sizeof(pixel_t), pixelCount, fp);
+}
+
+// Reads the entire PPM image, including header and pixel data.
+pixel_t* read(FILE* fp, header_t* header) {
+    readHeader(fp, header);
+    return readPixels(fp, *header);
+}
+
+// Writes the entire PPM image, including header and pixel data.
+void write(FILE* fp, header_t header, pixel_t* pixels) {
+    writeHeader(fp, header);
+    writePixels(fp, pixels, header);
+}
+
+// Allocates memory for the pixel data.
+pixel_t* allocatePixMemory(header_t header) {
+    return (pixel_t*) malloc(header.width * header.height * sizeof(pixel_t));
+}
+
+// Frees the memory used for pixel data.
+void freeMemory(pixel_t* pixels) {
+    free(pixels);
+}
